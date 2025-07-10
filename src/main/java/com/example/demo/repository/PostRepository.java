425ebc88f73f550087abf4a09d971335003dd24c
DU <sangdyjjang@naver.com>
2025-07-10 16:46:00 +0900

    chore.Explain about how to operate methods with nothing

--- src/main/java/com/example/demo/repository/PostRepository.java
+++ src/main/java/com/example/demo/repository/PostRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
-
+//아무 메서드도 없는데 동작하는 이유: extends JpaRepository<Post, Long>는 기본적인 CRUD 메서드를 제공하기 때문
 }
