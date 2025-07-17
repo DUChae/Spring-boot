154ef5fbede1e65a6aeb092380a8d38642c7702b
DU <sangdyjjang@naver.com>
2025-07-17 15:13:23 +0900

    feat.JPA Auditing

--- src/main/java/com/example/demo/domain/Post.java
+++ src/main/java/com/example/demo/domain/Post.java
@@ -8,12 +8,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,6 +33,12 @@ public class Post {
     @NotBlank(message = "작성자를 입력해주세요.")
     @Size(max=100,message="작성자 이름은 100자 이내로 입력해주세요.")
     private String author;
+
+
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
