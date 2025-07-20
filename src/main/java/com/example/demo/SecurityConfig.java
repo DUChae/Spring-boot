52ea2779eeaab03cdd55d060239c611632a6988e
DU <sangdyjjang@naver.com>
2025-07-20 16:27:34 +0900

    Feat.Login

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -1,10 +1,12 @@
-package com.example.demo.config;
+package com.example.demo;
 
 import com.example.demo.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
@@ -28,11 +30,17 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/posts", true)
                         .permitAll()
                 )
+                .csrf(csrf -> csrf.disable())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/login?logout")
                         .permitAll()
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
