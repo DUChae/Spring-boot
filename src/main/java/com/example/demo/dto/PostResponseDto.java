7f81ce7b3bdc7069b1b7daf7ff7b9451ed4ccad6
DU <sangdyjjang@naver.com>
2025-08-05 16:00:06 +0900

    feat: Add Dto

---
+++ src/main/java/com/example/demo/dto/PostResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.demo.dto;
+
+
+import com.example.demo.domain.Post;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String authorName;
+    private LocalDateTime createdAt;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.authorName = post.getAuthor().getUsername();
+        this.createdAt = post.getCreatedAt();
+    }
+}
