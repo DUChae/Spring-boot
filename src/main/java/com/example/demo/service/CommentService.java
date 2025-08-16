56f6610c3918851a993826182ef31fb73961aaf4
DU <sangdyjjang@naver.com>
2025-08-16 14:46:21 +0900

    Feat: Add and Delete function on Rest Api

--- src/main/java/com/example/demo/service/CommentService.java
+++ src/main/java/com/example/demo/service/CommentService.java
@@ -3,6 +3,8 @@ package com.example.demo.service;
 
 import com.example.demo.domain.Comment;
 import com.example.demo.domain.Post;
+import com.example.demo.domain.User;
+import com.example.demo.dto.CommentRequestDto;
 import com.example.demo.dto.CommentResponseDto;
 
 import java.util.List;
@@ -13,4 +15,7 @@ public interface CommentService {
     Comment findById(Long id);
     void deleteById(Long id);
     List<CommentResponseDto> getComments(Long postId);
+    Comment createComment(Long postId, CommentRequestDto requestDto, User user);
+    Comment updateComment(Long commentId, String newContent,User user);
+    void deleteComment(Long commentId, String username);
 }
