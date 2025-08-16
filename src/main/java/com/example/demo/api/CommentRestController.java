a14ed8547f51148c27b4777779f8103785403792
DU <sangdyjjang@naver.com>
2025-08-16 14:46:10 +0900

    Feat: Add and Delete function on Rest Api

--- src/main/java/com/example/demo/api/CommentRestController.java
+++ src/main/java/com/example/demo/api/CommentRestController.java
@@ -1,13 +1,16 @@
 package com.example.demo.api;
 
 
+import com.example.demo.domain.User;
+import com.example.demo.dto.CommentRequestDto;
 import com.example.demo.dto.CommentResponseDto;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.CommentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,13 +19,31 @@ import java.util.List;
 @RequestMapping("/api")
 public class CommentRestController {
     private final CommentService commentService;
-
+    private final UserRepository userRepository;
     //댓글 목록 조회
     @GetMapping("/posts/{postId}/comments")
     public List<CommentResponseDto> getComments(@PathVariable Long postId) {
         return commentService.getComments(postId);
     }
 
+    //댓글 등록
+    @PostMapping("/posts/{postId}/comments")
+    public ResponseEntity<Void> addComment(@PathVariable Long postId,
+                                           @RequestBody CommentRequestDto dto,
+                                           @AuthenticationPrincipal UserDetails userDetails){
+        User user= userRepository.findByUsername("test")
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없습니다."));
+        commentService.createComment(postId, dto, user);
+        return ResponseEntity.ok().build();
+    }
 
+
+    @DeleteMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable Long commentId,
+                                              @AuthenticationPrincipal UserDetails userDetails){
+        String username="test";
+        commentService.deleteComment(commentId, username);
+        return ResponseEntity.ok().build();
+    }
 
 }
